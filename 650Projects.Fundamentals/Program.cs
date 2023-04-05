24e847088160ca1f96d37c4a5980c7fd346d213c
Abbas-Davodkhani <davodkhaniabbas2003@gmail.com>
2023-04-05 09:51:02 +0430

    4

---
+++ 650Projects.Fundamentals/Program.cs
@@ -0,0 +1,36 @@
+﻿#region 1
+//int m, n;
+//char ch1 , ch2 ;
+//double d1 , d2 ;
+//float pl = 3.14f;
+#endregion
+
+#region 2
+//long x; // 10 bytes 
+//double d; // 10 bytes
+//char ch; // 1 byte
+//float f1 , f2; // 4 bytes
+#endregion
+
+#region 3
+// x = 5 , y = 4
+// m = x + (y*2) / 6 + y / 8;
+
+//int x = 5;
+//int y = 4;
+//int m = x + (y * 2) / 6 + y / 8;
+//m = 5 + 8 / 6 + y / 8;
+//m = 5 + 1 + 4 / 8;
+//m = 5 + 1 + 0;
+//Console.WriteLine(m);  // m = 6;
+#endregion
+
+#region 4
+// x = 4 , y = 3
+// m = x > y ? * 3 : y * 3;
+//int x = 4;
+//int y = 3;
+//int m = 4 > 3 ? 4 * 3 : 3 * 3;
+//m = 4 * 3;
+//Console.WriteLine(m);
+#endregion
